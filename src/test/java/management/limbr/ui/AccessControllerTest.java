b8e9e9f1cc78b19322c955d78581787a97abd7c9
Tyrel Haveman <tyrel@kulshanconcepts.com>
2016-02-27 11:31:30 -0800

    Create some more unit tests

---
+++ src/test/java/management/limbr/ui/AccessControllerTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2016 Tyrel Haveman and contributors.
+ *
+ * This file is part of Limbr.
+ *
+ * Limbr is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Limbr is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Limbr.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package management.limbr.ui;
+
+import org.springframework.context.ApplicationContext;
+import org.testng.annotations.Test;
+
+import static org.mockito.Mockito.*;
+import static org.testng.Assert.assertEquals;
+
+@Test
+public class AccessControllerTest {
+    public void grantsAccessBasedOnAnnotation() {
+        ApplicationContext mockContext = mock(ApplicationContext.class);
+        AccessController accessController = new AccessController();
+        accessController.setApplicationContext(mockContext);
+        VaadinUI mockUI = mock(VaadinUI.class);
+
+        @RequiresPrivilege(level = PrivilegeLevels.ADMIN)
+        class ABean {}
+        ABean aBean = new ABean();
+
+        when(mockContext.getBean("bean")).thenReturn(aBean);
+        when(mockUI.getUserLevel()).thenReturn(PrivilegeLevels.ADMIN);
+
+        assertEquals(accessController.isAccessGranted(mockUI, "bean"), true);
+
+        verify(mockContext).getBean("bean");
+        verify(mockUI).getUserLevel();
+    }
+}
