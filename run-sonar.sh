b2dc4bd6ee0dd201db9e9039b1334a8e47430c7b
Tyrel Haveman <tyrel@kulshanconcepts.com>
2016-02-28 11:53:16 -0800

    For #5, attempt to run sonar in preview mode on branches

--- run-sonar.sh
+++ run-sonar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ev
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" == "master" ]; then
     mvn --settings travis/settings.xml sonar:sonar -Dsonar.branch=`git name-rev --name-only HEAD`
 else
     mvn --settings travis/settings.xml sonar:sonar -Dsonar.branch=`git name-rev --name-only HEAD` -Dsonar.analysis.mode=preview -Dsonar.issuesReport.console.enable=true
